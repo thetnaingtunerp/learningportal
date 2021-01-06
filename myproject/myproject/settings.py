810a236f5bb4b316f9701938231eeb87a86483c2
kothet <kothetnaingtun.ict@gmail.com>
2021-01-06 09:18:56 +0630

    remove static root 6

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'static_cdn'
+# STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn/')
